d48e711f529f62edbd9538cc30b912b51aa58516
Abishek-45 <abishek13105@gmail.com>
2024-11-25 13:29:22 +0530

    created a repository and added necessary files and components

---
+++ learnhub/src/components/login_card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function login_card() {
+  return (
+    <div></div>
+  )
+}
+
+export default login_card
---
+++ learnhub/src/components/register_card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function register_card() {
+  return (
+    <div></div>
+  )
+}
+
+export default register_card
